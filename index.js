5c215e4fa7791b2cb08a088d3884dff85c6b9baa
Akassh <akash18011@gmail.com>
2024-07-03 15:25:18 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+app.use(express.json()); // Get Raw data from post API
+app.use(express.urlencoded({extended: true}));
+
+app.use((err, req, res, next) => { // body parser error catcher
+	if (err instanceof SyntaxError) {
+		res.status(400).json({status:400,msg:"Invalid Data send",data:null});
+	}else {
+		next();
+	}
+});
+
+app.get("/data",(req,res) => {
+	res.status(200).json({status:200,msg:"Success",data:null});
+});
+
+app.use("/*",(req,res) => {
+	res.status(404).json({status:404,msg:"Not Found",data:null});
+});
+
+app.listen(3000,(err) => {
+	if(!err) {
+		console.log("server running on 3000");
+	}
+	else {
+		console.log("Error while running the server!");
+	}
+});
