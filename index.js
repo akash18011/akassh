21d9daf7ad7a8bcba65f7b8bb17cb3557e8f2b3f
akash18011 <akash@prometteur.in>
2024-07-04 12:00:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-//updated file v6
+//updated file v7
 app.use(express.json()); // Get Raw data from post API
 app.use(express.urlencoded({extended: true}));
 
